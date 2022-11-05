92ff245d1bbccd6cc441efb25644af552c2f2812
binary-action55 <anirudhsundaram201@gmail.com>
2022-11-05 20:58:13 +0530

    password encrypt

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const rootDirectory = require('../utils/rootDirectory');
 const User = require(path.join(rootDirectory,'model','user'));
-
+const bcrypt = require('bcrypt');
 
 const SEQUELIZE_UNIQUE_ERROR = 'SequelizeUniqueConstraintError';
 
@@ -15,13 +15,15 @@ module.exports.addUser = async (req,res,next) =>{
     
     const name = req.body.name;
     const email = req.body.email;
-    const password = req.body.password;
+    
 
     try{
-        const item = await User.create({
+        const password = req.body.password;
+        const hash = await bcrypt.hash(password,10);
+        await User.create({
             name,
             email,
-            password,
+            password:hash,
         })
         res.status(201).json({success:true,message:'User Created',uniqueEmail:true});
     }
@@ -50,11 +52,12 @@ module.exports.checkLogin = async (req,res,next) =>{
                  email:userName,
              }
          })
-         if(items.length==0)
+         if(items.length===0)
              return res.status(404).json({message:'User not found',userNameValid:false});
-         if(items[0].password!==password)
-             return res.status(400).json({message:'User not Authorized',userNameValid:true,passwordValid:false});
-         res.status(201).json({success:true,message:'User Login Successful',userNameValid:true,passwordValid:true});
+         const result = await bcrypt.compare(password,items[0].password);
+         if(!result)
+            return res.status(400).json({message:'User not Authorized',userNameValid:true,passwordValid:false});
+         res.status(200).json({success:true,message:'User Login Successful',userNameValid:true,passwordValid:true});
      }
      catch(err){
          res.status(500).json({message:err});
